e813edda14d12481a6463cf8406eff49933ac181
Walter Gantenbein <gantenbeinw@bluewin.ch>
2018-01-18 23:15:12 +0100

    Gradle Plugin & Build Tool Update

--- app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,6 +15,10 @@ import android.test.ApplicationTestCase;
 
 public class ExampleInstrumentedTest extends ApplicationTestCase<Application> {
     public ExampleInstrumentedTest() {
+
         super(Application.class);
+
+
     }
+
 }
